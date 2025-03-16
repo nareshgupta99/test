6fd13adbd87375fcb6a99d218cf2fb73a8ddd46a
Naresh Gupta <nareshgupta0899@gmail.com>
2025-03-16 19:14:47 +0530

    Revert "binary search code starting"
    This reverts commit e40ea3fd64c079a5de9e6c2566d342b459e4b90d.

--- test.java
+++ test.java
@@ -4,13 +4,4 @@ class test{
         System.out.println("hello");
     }
 
-    public void binarySerach(int arr,int k){
-
-        int s=0,e=arr-1,ans=-1;
-        while (s<=e) {
-            int mid=(s+e)/2;
-            
-        }
-
-    }
 }
